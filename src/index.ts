7854a73e59b9ce9c90543956e7529c77c1276fb2
Philipp Daun <post@philippdaun.net>
2023-07-04 22:01:30 +0200

    Type options

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
 import type { AstroIntegration } from 'astro';
 import { buildInitScript } from './script';
 
-export default function createPlugin(): AstroIntegration {
+export interface Options {
+	containers: string[],
+	animationClass: string,
+	cache: boolean,
+	debug: boolean,
+	accessibility: boolean,
+	preload: boolean,
+	progress: boolean,
+	routes: false | any[],
+	smoothScrolling: boolean,
+	reloadScripts: boolean,
+	updateBodyClass: boolean,
+	updateHead: boolean,
+	theme: 'fade' | 'slide' | 'overlay' | false
+};
+
+export default function createPlugin(options?: Options): AstroIntegration {
 	return {
 		name: '@swup/astro',
 		hooks: {
 			'astro:config:setup': ({ injectScript }) => {
-				const script = buildInitScript();
+				const script = buildInitScript(options);
 				injectScript('page', script);
 			},
 		},
