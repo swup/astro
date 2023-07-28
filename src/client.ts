a5180f5758dd43a758b77fc8d2c59206c7435d89
Philipp Daun <post@philippdaun.net>
2023-07-28 16:05:05 +0200

    Order imports

--- src/client.ts
+++ src/client.ts
@@ -1,16 +1,16 @@
 import Swup from 'swup';
-import SwupDebugPlugin from '@swup/debug-plugin';
 import SwupA11yPlugin from '@swup/a11y-plugin';
+import SwupBodyClassPlugin from '@swup/body-class-plugin';
+import SwupDebugPlugin from '@swup/debug-plugin';
+import SwupFadeTheme from '@swup/fade-theme';
+import SwupHeadPlugin from '@swup/head-plugin';
+import SwupOverlayTheme from '@swup/overlay-theme';
 import SwupPreloadPlugin from '@swup/preload-plugin';
 import SwupProgressPlugin from '@swup/progress-plugin';
-import SwupScrollPlugin from '@swup/scroll-plugin';
-import SwupScriptsPlugin from '@swup/scripts-plugin';
-import SwupBodyClassPlugin from '@swup/body-class-plugin';
-import SwupHeadPlugin from '@swup/head-plugin';
 import SwupRouteNamePlugin from '@swup/route-name-plugin';
-import SwupFadeTheme from '@swup/fade-theme';
+import SwupScriptsPlugin from '@swup/scripts-plugin';
+import SwupScrollPlugin from '@swup/scroll-plugin';
 import SwupSlideTheme from '@swup/slide-theme';
-import SwupOverlayTheme from '@swup/overlay-theme';
 
 /**
  * This would be great as a client script for dynamic importing.
@@ -20,16 +20,16 @@ import SwupOverlayTheme from '@swup/overlay-theme';
  */
 export {
   Swup,
-  SwupDebugPlugin,
   SwupA11yPlugin,
+  SwupBodyClassPlugin,
+  SwupDebugPlugin,
+  SwupFadeTheme,
+  SwupHeadPlugin,
+  SwupOverlayTheme,
   SwupPreloadPlugin,
   SwupProgressPlugin,
-  SwupScrollPlugin,
-  SwupScriptsPlugin,
-  SwupBodyClassPlugin,
-  SwupHeadPlugin,
   SwupRouteNamePlugin,
-  SwupFadeTheme,
+  SwupScriptsPlugin,
+  SwupScrollPlugin,
   SwupSlideTheme,
-  SwupOverlayTheme
 }
