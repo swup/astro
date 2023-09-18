d058b8ebf130ee836607006f3d103614560e061b
Philipp Daun <post@philippdaun.net>
2023-09-18 22:59:36 +0200

    Serialise regular expressions

--- src/serialise.ts
+++ src/serialise.ts
@@ -21,6 +21,9 @@ export function deserialise(serialised: string) {
 }
 
 function replacer(key: string, value: unknown) {
+	if (value instanceof RegExp) {
+		return [':regex:', value.toString()];
+	}
 	if (typeof value === 'function') {
 		let output = value.toString();
 		if (value.name && (new RegExp(`^\\s*${value.name}\\s*[(]`)).test(output)) {
@@ -32,9 +35,16 @@ function replacer(key: string, value: unknown) {
 }
 
 function reviver(key: string, value: unknown) {
-	if (Array.isArray(value) && value[0] === ':function:' && typeof value[1] === 'string') {
+	if (Array.isArray(value) && value.length === 2 && typeof value[1] === 'string') {
+		const type = value[0];
 		value = value[1];
-		return new Function(`return (${value}).apply(this, arguments);`);
+		if (type === ':regex:') {
+			const fragments = (value as string).match(/\/(.*?)\/([a-z]*)?$/i) || [];
+			return new RegExp(fragments[1], fragments[2] || '');
+		}
+		if (type === ':function:') {
+			return new Function(`return (${value}).apply(this, arguments);`);
+		}
 	}
 	return value;
 }
